f265a26651ee6565ed9330d3c888f56f57edda75
lakshmiprasadlp <soetemp2300@gmail.com>
2023-12-04 16:17:37 +0530

    updated files

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # ml_ops_project
 
-# requriment.txt
+# to creat env
+```
+conda create -p venv python=3.8 -y
+
+```
+# to acatavate env
+```
+conda activate venv\
+```
+
+# install requriment.txt
 ```
 pip install -r requirements.txt
 ```
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+REPO_NAME = "ml_ops_project"
+AUTHOR_USER_NAME = "lakshmiprasadlp"
+SRC_REPO = "ml_ops_project"
+AUTHOR_EMAIL = "soetemp2300@gmail.com"
 
 
 setuptools.setup(
